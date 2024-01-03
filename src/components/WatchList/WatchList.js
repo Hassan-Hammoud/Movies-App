92cf4a856ceb7f862aec713f92844609db7b7b56
Hassan-Hammoud <hassanhammoud2000@gmail.com>
2024-01-03 23:10:49 +0200

    Finished Watched

--- src/components/WatchList/WatchList.js
+++ src/components/WatchList/WatchList.js
@@ -1,7 +1,7 @@
 import React from "react";
-import {useMovieContext} from "../context/GlobalContext"
+import { useMovieContext } from "../context/GlobalContext";
 import MovieCard from "../MovieCard/MovieCard";
-import './WatchList.css';
+import "./WatchList.css";
 const WatchList = () => {
   const MovieContext = useMovieContext();
   return (
@@ -9,13 +9,20 @@ const WatchList = () => {
       <div className="container">
         <div className="main-heading">
           <h2>My Watch List</h2>
-          <p className="movies-count">2 Movies</p>
+          <p className="movies-count">
+            {MovieContext.watchList.length}{" "}
+            {MovieContext.watchList.length <= 1 ? "Movie" : "Movies"}
+          </p>
         </div>
-        {MovieContext.watchList.length > 0 ? (<div className="movie-grid">
-          {MovieContext.watchList.map((movie) => (
-            <MovieCard key={movie.imdbID} movie={movie} type="watchList"/>
-          ))}
-        </div>) : (<h3>There Is No Available List Of Movies, Add Some! </h3>)}
+        {MovieContext.watchList.length > 0 ? (
+          <div className="movie-grid">
+            {MovieContext.watchList.map((movie) => (
+              <MovieCard key={movie.imdbID} movie={movie} type="watchList" />
+            ))}
+          </div>
+        ) : (
+          <h3>There Is No Available List Of Movies, Add Some! </h3>
+        )}
       </div>
     </div>
   );
--- src/components/Watched/Watched.js
+++ src/components/Watched/Watched.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import { useMovieContext } from "../context/GlobalContext";
+import MovieCard from "../MovieCard/MovieCard";
 
 const Watched = () => {
+  const MovieContext = useMovieContext();
   return (
-    <div>
-      Hello watched
+    <div className="watch-list">
+      <div className="container">
+        <div className="main-heading">
+          <h2>My Watched </h2>
+          <p className="movies-count">
+            {MovieContext.watched.length}{" "}
+            {MovieContext.watched.length <= 1 ? "Movie" : "Movies"}
+          </p>
+        </div>
+        {MovieContext.watched.length > 0 ? (
+          <div className="movie-grid">
+            {MovieContext.watched.map((movie) => (
+              <MovieCard key={movie.imdbID} movie={movie} type="watched" />
+            ))}
+          </div>
+        ) : (
+          <h3>No Watched Movies</h3>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Watched
+export default Watched;
